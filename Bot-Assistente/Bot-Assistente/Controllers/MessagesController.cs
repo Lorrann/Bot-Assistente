803de47da7197b3b7eb15b29290ab052ffd37aae
Lorrann Corrêa <lorrann.correa@esx.com.br>
2017-09-05 08:37:50 -0300

    Mudando conexao com o luis

--- Bot-Assistente/Bot-Assistente/Controllers/MessagesController.cs
+++ Bot-Assistente/Bot-Assistente/Controllers/MessagesController.cs
@@ -9,6 +9,8 @@ using System.Configuration;
 using Bot_Assistente.Dialogs;
 using System;
 using Bot_Assistente.Controllers;
+using System.Linq;
+using Microsoft.Bot.Builder.Luis;
 
 namespace Bot_Assistente
 {
@@ -21,54 +23,55 @@ namespace Bot_Assistente
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            var atributes = new LuisModelAttribute(
+                    ConfigurationManager.AppSettings["LuisId"],
+                    ConfigurationManager.AppSettings["LuisKey"]);
+
+            var service = new LuisService(atributes);
 
             if (activity.Type == ActivityTypes.Message)
             {
-                await InstrumentationHelper.DefaultInstrumentation.TrackActivity(activity);
-
-                await Conversation.SendAsync(activity, () => new RootDialog(
-                    ConfigurationManager.AppSettings["LuisId"],
-                    ConfigurationManager.AppSettings["LuisKey"]));
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog(service));
             }
             else
             {
-                var reply = activity.CreateReply();
-                reply.Text = "Olá, eu sou o **BotInho**.\n\n\nPor hora a única coisa que eu faço é jogar um **RPG** com você.\n\n\nMas lembre-se, eu sou o master. É só você seguir os meus comandos que vamos ter um bom jogo.\n\n\n**PS:** Eu estou na v0.1.1, então pega leve tá?!?";
-
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await HandleSystemMessageAsync(activity);
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        //private Activity HandleSystemMessage(Activity message)
-        //{
-        //    if (message.Type == ActivityTypes.DeleteUserData)
-        //    {
-        //        // Implement user deletion here
-        //        // If we handle user deletion, return a real message
-        //    }
-        //    else if (message.Type == ActivityTypes.ConversationUpdate)
-        //    {
-        //        // Handle conversation state changes, like members being added and removed
-        //        // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-        //        // Not available in all channels
-        //    }
-        //    else if (message.Type == ActivityTypes.ContactRelationUpdate)
-        //    {
-        //        // Handle add/remove from contact lists
-        //        // Activity.From + Activity.Action represent what happened
-        //    }
-        //    else if (message.Type == ActivityTypes.Typing)
-        //    {
-        //        // Handle knowing tha the user is typing
-        //    }
-        //    else if (message.Type == ActivityTypes.Ping)
-        //    {
-        //    }
+        private async Task<Activity> HandleSystemMessageAsync(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
 
-        //    return null;
-        //}
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (message.MembersAdded.All(o => o.Id != message.Recipient.Id))
+                    return null;
+
+                var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+                var reply = message.CreateReply();
+                reply.Text = "Olá, sou Alice em que posso ajudar?";
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+
+            }
+
+            return null;
+        }
     }
 }
--- Bot-Assistente/Bot-Assistente/Dialogs/RootDialog.cs
+++ Bot-Assistente/Bot-Assistente/Dialogs/RootDialog.cs
@@ -3,13 +3,14 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis.Models;
 using BotBuilder.Instrumentation.Dialogs;
+using Microsoft.Bot.Builder.Luis;
 
 namespace Bot_Assistente.Dialogs
 {
     [Serializable]
-    public class RootDialog : InstrumentedLuisDialog<object>
+    public class RootDialog : LuisDialog<object>
     {
-        public RootDialog(string luisModelId, string luisSubscriptionKey) : base(luisModelId, luisSubscriptionKey) { }
+        public RootDialog(LuisService service) : base(service) { }
 
         #region Intents
         
@@ -59,26 +60,5 @@ namespace Bot_Assistente.Dialogs
         }
 
         #endregion
-
-
-        //public Task StartAsync(IDialogContext context)
-        //{
-        //    context.Wait(MessageReceivedAsync);
-
-        //    return Task.CompletedTask;
-        //}
-
-        //private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
-        //{
-        //    var activity = await result as Activity;
-
-        //    // calculate something for us to return
-        //    int length = (activity.Text ?? string.Empty).Length;
-
-        //    // return our reply to the user
-        //    await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-        //    context.Wait(MessageReceivedAsync);
-        //}
     }
 }
